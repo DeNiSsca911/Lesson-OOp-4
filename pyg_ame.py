5f09310e1ec402850ac71f8bc3a630e9ce3bb869
DeNiSsca911 <denwertyu@gmail.com>
2025-04-07 19:14:44 +0300

    suv

--- pyg_ame.py
+++ pyg_ame.py
@@ -2,7 +2,6 @@ from operator import truediv
 
 import pygame
 pygame.init()
-import time
 
 window_size = (800,600)
 screen = pygame.display.set_mode(window_size)
@@ -11,8 +10,7 @@ pygame.display.set_caption("Текстовый проект")
 image = pygame.image.load("sm.png")
 image_rect = image.get_rect()
 
-image2 = pygame.image.load("pit.png")
-image_rect2 = image2.get_rect()
+speed = 1
 
 run = True
 
@@ -20,19 +18,20 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEMOTION:
-            mouseX, mouseY = pygame.mouse.get_pos()
-            image_rect.x = mouseX - 30
-            image_rect.y = mouseY - 30
 
-    if image_rect.colliderect(image_rect2):
-        print("Произошло столкновение !")
-        time.sleep(1)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        image_rect.x -= speed
+    if keys[pygame.K_RIGHT]:
+        image_rect.x += speed
+    if keys[pygame.K_UP]:
+        image_rect.y -= speed
+    if keys[pygame.K_DOWN]:
+        image_rect.y += speed
 
 
     screen.fill((0,0,0))
     screen.blit(image,image_rect)
-    screen.blit(image2, image_rect2)
     pygame.display.flip()
 
 pygame.QUIT
